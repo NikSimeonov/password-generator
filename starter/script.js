542961dd66b548d647eb2cbf02336a6ff12c5687
NikSimeonov <nikolay.simeonov89@gmail.com>
2022-11-29 10:52:27 +0000

    logic added for the different password options

--- starter/script.js
+++ starter/script.js
@@ -91,7 +91,66 @@ var upperCasedCharacters = [
 // Function to prompt user for password options
 function getPasswordOptions() {
 
-}
+
+  //prompt the user to enter a password between 10 and 64 characters.
+  var passwordLength = prompt('Please enter a number between 10 and 64 to generate your password');
+
+  if (passwordLength < 10 || passwordLength > 64) {
+    alert('Please enter a valid number between 10 and 64');
+  } else if (isNaN(passwordLength)) {
+    passwordLength = prompt('Please enter a valid number');
+  } else {
+    alert('Your password will be ' + passwordLength + ' characters long');
+  };
+  
+  //asking the user if he/she would like to include lowercase characters.
+  var lower = confirm('Would you like your password to contain lower case letters?');
+
+  if(lower) {
+    alert('Your password will have lowercase characters');
+  } else {
+    alert('Your password will NOT have lowercase characters');
+  };
+
+  //asking the user if he/she would like to include uppercase characters.
+  var upper = confirm('Would you like your password to contain upper case letters?');
+
+  if(upper) {
+    alert('Your password will have uppercase characters');
+  } else {
+    alert('Your password will NOT have uppercase characeters');
+  };
+
+
+  //asking the user if he/she would like to include special characters.
+  var spec = confirm('Would you like your password to contain special characters?');
+
+  if(spec) {
+    alert('Your password will have special characters');
+  } else {
+    alert('Your password will NOT have special characters');
+  };
+
+
+  //asking the user if he/she would like to include numbers in the password
+  var numbers = confirm('Would you like your password to contain numbers?');
+
+  if(numbers) {
+    alert('Your password will have numbers')
+  } else {
+    alert('Your password will NOT have numbers');
+  };
+
+
+  //checking if at least one character type is selected if not alerting to do so.
+  if (lower === false && upper === false && spec === false && numbers === false){
+    alert('! Please select at least one character type !');
+  };
+
+
+    return [lower, upper, spec, numbers, passwordLength];
+
+};
 
 // Function for getting a random element from an array
 function getRandom(arr) {
@@ -100,7 +159,11 @@ function getRandom(arr) {
 
 // Function to generate password with user input
 function generatePassword() {
-
+  
+ var passDetails = getPasswordOptions ();
+ if(passwordLength <= 10 || passwordLength >= 64) {
+  alert('You have entered a invalid number! Please choose a number between 5 and 64');
+ }
 }
 
 // Get references to the #generate element
